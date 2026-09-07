df5b9a80893f1a173b34d0fffc86f4aef8aa99eb
lakshya <lakshyarampal60@gmail.com>
2026-09-08 00:51:56 +0530

    day 2

---
+++ 10.py
@@ -0,0 +1,8 @@
+#if else statement
+
+age=int(input("enter your age: "))
+
+if age>=18:
+    print("you are an adult")
+else:
+    print("you are a minor")
--- 8.py
+++ 8.py
@@ -1,2 +1,16 @@
-#streak
-#strek
+import math 
+
+x=9
+
+
+print(math.pi)
+print(math.e)
+
+result=math.sqrt(x) #returns the square root of a number
+print(result)
+
+result=math.ceil(x) #rounds up to the nearest integer
+print(result)
+
+result=math.floor(x) #rounds down to the nearest integer
+print(result)   
